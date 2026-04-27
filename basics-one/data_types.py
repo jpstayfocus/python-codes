c8ee1545926e7beb97cb8ee4fb0f195ef69d894d
jpstayfocus <jpstayfocus@gmail.com>
2026-04-27 11:21:59 -0400

    basics

---
+++ basics-one/data_types.py
@@ -0,0 +1,12 @@
+# fundamental data types : values in python
+
+# Integer: a number: 3 or 5 (we can do mathematical operation)
+print(type(6))
+print(type(2 * 4))
+print(type(2 - 4))
+# 0.5 is a float: number with decimal point. (float needs  more memory)
+print(type(2 / 4))
+
+print(2 ** 3)  # 2 to the power of 3
+print(5 // 4)  # rouded down to integer
+print(5 % 4)  # returns 1 -> remainder of the division
